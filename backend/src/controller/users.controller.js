0725d77fd5e666a3d3ccd3a89f449c7ee8a7a934
Shlomi <shlomikaufman91@gmail.com>
2026-09-17 11:24:41 +0300

    feat: add login controller

--- backend/src/controller/users.controller.js
+++ backend/src/controller/users.controller.js
@@ -1,7 +1,19 @@
-import { signup } from "../services/users.service";
+import { login, signup } from "../services/users.service";
 
 async function signupRoute(req, res) {
   const signed = await signup(req.body);
   res.status(201).send({ message: "User registered successfully" });
 }
 
+async function loginRoute(req, res) {
+  const token = await login(req.body);
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: "true",
+    sameSite: "strict",
+  });
+  res.status(201).send({ message: "user login successfully", token });
+}
+
+export { signupRoute, loginRoute };
