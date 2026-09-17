b467af5b94acd4a50ba92a0712fa3365116a550a
Shlomi <shlomikaufman91@gmail.com>
2026-09-17 11:05:34 +0300

    feat: add login at service

--- backend/src/services/users.service.js
+++ backend/src/services/users.service.js
@@ -1,18 +1,20 @@
-import { User } from "../schema/user";
+import { UserSignup, UserLogin } from "../schema/user";
 import jwt from "jsonwebtoken";
 import bcrypto from "bcryptjs";
-import { addUser, getAll } from "../repositories/users";
+import { addUser, findByEmail, findById, getAll } from "../repositories/users";
+import { EXPIRE, SECRET } from "../config";
 
 async function signup(user) {
-  const parsed = User.safeParse(user);
+  const parsed = UserSignup.safeParse(user);
   if (!parsed.success) {
-    throw Object.assign(new Error(parsed.error.issues), 422);
+    throw Object.assign(new Error(parsed.error.issues), { status: 422 });
   }
 
   const { username, email, password } = parsed.data;
   const users = await getAll();
-  if (users.includes((user) => user.email === email)) {
-    throw Object.assign(new Error("Email not avalible"), 409);
+  const user = await findByEmail(email);
+  if (user) {
+    throw Object.assign(new Error("Email not avalible"), { statous: 409 });
   }
 
   const passwordHash = await bcrypto.hash(password, 12);
@@ -23,3 +25,30 @@ async function signup(user) {
   return saveUser;
 }
 
+async function login(user) {
+  const parsed = UserLogin.safeParse(user);
+  if (!parsed.success) {
+    throw Object.assign(new Error(parsed.error.issues), { status: 422 });
+  }
+
+  const { email, password } = parsed.data;
+  const user = await findByEmail(email);
+
+  if (!user) {
+    throw Object.assign(new Error("User not found"), { status: 404 });
+  }
+
+  const isMatch = await bcrypto.compare(password, user.password);
+
+  if (!isMatch) {
+    throw Object.assign(new Error("Not currect email or password"), {
+      status: 403,
+    });
+  }
+
+  const payload = { id: user.id, username: user.username, email: user.email };
+  const token = jwt.sign(payload, SECRET, { expiresIn: EXPIRE });
+  return token;
+}
+
+export { signup, login };
